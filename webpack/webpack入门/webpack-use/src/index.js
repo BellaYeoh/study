04d9038540292d57abcf3495f5fe1c30265fec07
yanyoeh <yy1336316@126.com>
2020-12-17 20:27:01 +0800

    webpack的简单使用demo

---
+++ webpack/webpack入门/webpack-use/src/index.js
@@ -0,0 +1,10 @@
+import './main.css';
+
+function createEl() {
+  var element = document.createElement('div')
+  element.innerHTML = 'hello world'
+
+  return element;
+}
+
+document.body.appendChild(createEl());
